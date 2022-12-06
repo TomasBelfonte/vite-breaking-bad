99b65106af15d0415e46fb38a3311141fb4f8104
TomasBelfonte <tomas.belfonte@gmail.com>
2022-12-06 21:37:56 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <TheMain></TheMain>
+  </div>
+</template>
+
+<script>
+  import TheMain from './components/TheMain.vue';
+
+  export default {
+    components: {TheMain}
+  }
+</script>
+
+<style lang="scss">
+@use "./style/general.scss";
+
+</style>
---
+++ src/components/TheMain.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    import: axios, from: "axios",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
